806787a980be882642fffe43544cf8d7128f5d4d
Henrique Martins <enrque.ns@gmail.com>
2019-02-22 15:26:48 -0300

    binary mask to /24

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -9,40 +9,80 @@ class App extends Component {
         this.state = {
             address: '',
             addressBin: '',
-            maskBin: '',
+            mask: '',
             mask255: '',
-            mask: ''
+            maskBin: '',
+            Wildcard: ''
         };
     }
 
-    handleAddress = async ({ target }) => {
-        await this.setState({ address: target.value });
-        this.handleAddressBin();
-    };
-    
-    handleAddressBin = () => {
-        const { address } = this.state;
-        const addressBin = address
-            .split('.')
-            .map(block => new Number(block).toString(2))
-            .reduce((final, now) => `${final}.${now}`);
+    convertIp = (address, base) => {
+        const convert = block =>
+            base === 10 ? parseInt(block, 2) : (+block).toString(2);
 
-        this.setState({ addressBin });
+        return address
+            .split('.')
+            .map(block => convert(block))
+            .reduce((final, now) => `${final}.${now}`);
+    };
+
+    handleAddress = async ({ target }) => {
+        if (target) {
+            await this.setState({ address: target.value });
+            this.handleAddressBin({});
+        } else {
+            const address = this.convertIp(this.state.addressBin, 10);
+            this.setState({ address });
+        }
+    };
+
+    handleAddressBin = async ({ target }) => {
+        if (target) {
+            await this.setState({ addressBin: target.value });
+            this.handleAddress({});
+        } else {
+            const addressBin = this.convertIp(this.state.address, 2);
+            this.setState({ addressBin });
+        }
     };
 
     handleMask = async ({ target }) => {
-        await this.setState({ mask: target.value });
-        this.handleMaskBin();
+        if (target) {
+            await this.setState({ mask: target.value });
+            this.handleMaskBin({}, 'mask');
+        } else {
+            const maskBin = this.state.maskBin.split('.').join('');
+            const mask = maskBin.length - maskBin.split('1').join('').length;
+            await this.setState({ mask });
+        }
     };
 
-    handleMaskBin = () => {
-        const { mask } = this.state;
+    handleMaskBin = async ({ target }, origin) => {
+        if (target) {
+            await this.setState({ maskBin: target.value });
+            this.handleMask({});
+        } else {
+            const { mask } = this.state;
+            let maskBin;
 
-        const bin = `${'1'.repeat(mask)}${'0'.repeat(32 - mask)}`;
-        const maskBin = `${bin.substr(0,8)}.${bin.substr(8,8)}.${bin.substr(16,8)}.${bin.substr(24,8)}`;
+            if (origin === 'mask255'){
+            } else {
+                const bin = `${'1'.repeat(mask)}${'0'.repeat(32 - mask)}`;
+                maskBin = `${bin.substr(0, 8)}.${bin.substr(8, 8)}.${bin.substr(16, 8)}.${bin.substr(24, 8)}`;
+            }
 
-        this.setState({ maskBin });
+            await this.setState({ maskBin });
+        }
     };
+
+    handleMask255 = async ({ target }) => {
+        if (target) {
+            await this.setState({ mask255: target.value });
+        } else {
+            const mask255 = this.convertIp(this.state.maskBin, 10);
+            this.setState({ mask255 });
+        }
+    }
 
     render() {
         return (

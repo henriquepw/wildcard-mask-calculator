6a5ae1a1cb49d27a4f8a61833d09766c64584183
Henrique Martins <enrque.ns@gmail.com>
2019-03-02 17:09:44 -0300

    Updates

---
+++ precache-manifest.dd67333e981d92eaa48a81b33248f203.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "42c4ae3eb2a090e9b337",
+    "url": "/static/js/main.42c4ae3e.chunk.js"
+  },
+  {
+    "revision": "eea1473c73a60d8d7d67",
+    "url": "/static/js/2.eea1473c.chunk.js"
+  },
+  {
+    "revision": "8b2c0ee439f948559194aa2ead173893",
+    "url": "/index.html"
+  }
+];

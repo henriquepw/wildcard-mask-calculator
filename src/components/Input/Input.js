afbf78d2c567fd15a7447f6c1142021c1e24ee5e
Henrique Martins <enrque.ns@gmail.com>
2019-03-02 10:31:56 -0300

    validator

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Container, Input } from './Input.style';
 
 export default props => (
-    <Container flex={props.flex} right={props.right}>
-        <label>{props.name}</label>
-        <Input {...props.input} />
-    </Container>
+	<Container flex={props.flex} right={props.right}>
+		<label>{props.name}</label>
+		<Input {...props.input} />
+	</Container>
 );
--- src/styles/grid.js
+++ src/styles/grid.js
@@ -1,14 +1,14 @@
 import styled, { css } from 'styled-components';
 
 const Box = styled.main`
-    width: 60vw;
-    height: 100%;
-    min-width: 761px;
-    max-width: 900px;
+	width: 60vw;
+	height: 100%;
+	min-width: 761px;
+	max-width: 900px;
 
-    display: grid;
-    grid-template-columns: 1fr 3em 1fr;
-    grid-template-rows: 1fr repeat(6, auto) 1fr;
+	display: grid;
+	grid-template-columns: 1fr 3em 1fr;
+	grid-template-rows: 1fr repeat(6, auto) 1fr;
 `;
 
 const Cell = styled.div`
@@ -18,24 +18,28 @@ const Cell = styled.div`
     padding-bottom: 1.5em;
 
     ${({ rowAll }) =>
-        rowAll && css`
-            grid-column: 1 / span 3;
-        `}
+		rowAll &&
+		css`
+			grid-column: 1 / span 3;
+		`}
 
     ${({ right }) =>
-        right && css`
-            grid-column-start: 3;
-        `}
+		right &&
+		css`
+			grid-column-start: 3;
+		`}
 
     ${({ left }) =>
-        left && css` 
-            justify-content: right;
-        `}
-        
+		left &&
+		css`
+			justify-content: right;
+		`}
+
     ${({ center }) =>
-        center && css`
-            justify-content: center;
-        `}
+		center &&
+		css`
+			justify-content: center;
+		`}
 `;
 
 export { Box, Cell };

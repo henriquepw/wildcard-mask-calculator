9791047a7c0e5183252a5af35f07f33b150616f0
Henrique Martins <enrque.ns@gmail.com>
2019-03-02 17:10:32 -0300

    Updates

---
+++ precache-manifest.205336baabafad53119ebf20a78abb9b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4c4a482bd02ba2e6219",
+    "url": "/wildcard-mask-calculator/static/js/runtime~main.e4c4a482.js"
+  },
+  {
+    "revision": "8848dfcaea7ee867b3fa",
+    "url": "/wildcard-mask-calculator/static/js/main.8848dfca.chunk.js"
+  },
+  {
+    "revision": "d1e10d6fca56f502aea5",
+    "url": "/wildcard-mask-calculator/static/js/2.d1e10d6f.chunk.js"
+  },
+  {
+    "revision": "2a8346fadd0308f910983f4d3e87b5b8",
+    "url": "/wildcard-mask-calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd67333e981d92eaa48a81b33248f203.js"
+  "/wildcard-mask-calculator/precache-manifest.205336baabafad53119ebf20a78abb9b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/wildcard-mask-calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
